1354f8a7c6c7293f3b5bc53618eddfd16dd786e3
Julian Hecker <julianheckerdev@gmail.com>
2021-02-27 15:40:08 -0500

    fix up the tabbing functionality, fix todo css

--- popup.js
+++ popup.js
@@ -29,10 +29,39 @@ window.addEventListener('load', (e) => {
 
 
 
+/* Todo Tab */
+const enterButton = document.getElementById('enter-button');
+const inputBar = document.getElementById('input-bar');
+const ul = document.getElementById('todo-list-ul');
 
+const CreateNewListItem = () => {
+    const listItem = document.createElement('li');
+    const listItemContent = document.createTextNode(inputBar.value);
+    listItem.appendChild(listItemContent);
+    ul.appendChild(listItem);
+    inputBar.value = '';
+    listItem.addEventListener('click', () => {
+        listItem.style.textDecoration = 'line-through';
+    });
+    const delButton = document.createElement('button');
+    delButton.appendChild(document.createTextNode('X'));
+    listItem.appendChild(delButton);
+    delButton.addEventListener('click', () => {
+        listItem.removeChild(delButton);
+        ul.removeChild(listItem);
+    });
+};
 
+enterButton.addEventListener('click', () => {
+    // Execute only if user's input is not blank
+    if (inputBar.value.length > 0) {
+        CreateNewListItem();
+    }
+});
 
-
-
-
-
+inputBar.addEventListener('keypress', (Event) => {
+    // Execute only if user's input is not blank and if the key code is 13:
+    if (inputBar.value.length > 0 && Event.keyCode === 13) {
+        CreateNewListItem();
+    }
+});
