f83dfaa5b77db3e9334508db021cc2fdcfc15a61
Julian Hecker <julianheckerdev@gmail.com>
2021-02-26 23:06:07 -0500

    add files

---
+++ popup.js
@@ -0,0 +1,8 @@
+const todoButton = document.getElementById("todo-button");
+const timerButton = document.getElementById("timer-button");
+const sitesButton = document.getElementById("sites-button");
+const todoTab = document.getElementById("todo-tab");
+const timerTab = document.getElementById("timer-tab");
+const sitesTab = document.getElementById("sites-tab");
+
+console.log("REEEEEEEEEEEEEEEEE");
