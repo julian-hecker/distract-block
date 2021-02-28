ae88f54c0a98c016a5d243beb350a8172e6a09e9
Julian Hecker <julianheckerdev@gmail.com>
2021-02-27 22:19:24 -0500

    fix todos to work with chrome storage

--- popup.js
+++ popup.js
@@ -24,44 +24,71 @@ window.addEventListener('load', (e) => {
     for (let button of [todoButton, timerButton, sitesButton]) {
         button.addEventListener('click', handleChangeTab);
     }
+    loadTodos();
 });
-
-
-
 
 /* Todo Tab */
 const enterButton = document.getElementById('enter-button');
 const inputBar = document.getElementById('input-bar');
 const ul = document.getElementById('todo-list-ul');
+let todos = [];
 
-const CreateNewListItem = () => {
+const loadTodos = () => {
+    chrome.storage.sync.get(['todos'], (res) => {
+        todos = res.todos;
+        for (todo of todos) {
+            CreateNewListItem(todo);
+        }
+    });
+};
+
+const handleSubmitTodo = (e) => {
+    const todoText = inputBar.value;
+    todos.push(todoText);
+    chrome.storage.sync.set({ todos: todos }, function (response) {
+        console.log(response);
+    });
+    CreateNewListItem();
+};
+
+const handleRemoveTodo = (e) => {
+    const itemToRemove = e.target.parentNode;
+    itemToRemove.remove();
+    todos = Array.from(ul.querySelectorAll('li')).map(
+        (listItem) => listItem.innerText,
+    );
+    chrome.storage.sync.set({ todos: todos }, function (response) {
+        console.log(response);
+    });
+};
+
+const CreateNewListItem = (value = null) => {
     const listItem = document.createElement('li');
-    const listItemContent = document.createTextNode(inputBar.value);
+    const listItemContent = document.createTextNode(
+        inputBar.value ? inputBar.value : value,
+    );
     listItem.appendChild(listItemContent);
     ul.appendChild(listItem);
     inputBar.value = '';
     listItem.addEventListener('click', () => {
-        listItem.style.textDecoration = 'line-through';
+        listItem.classList.toggle('completed');
     });
     const delButton = document.createElement('button');
     delButton.appendChild(document.createTextNode('X'));
     listItem.appendChild(delButton);
-    delButton.addEventListener('click', () => {
-        listItem.removeChild(delButton);
-        ul.removeChild(listItem);
-    });
+    delButton.addEventListener('click', handleRemoveTodo);
 };
 
 enterButton.addEventListener('click', () => {
     // Execute only if user's input is not blank
     if (inputBar.value.length > 0) {
-        CreateNewListItem();
+        handleSubmitTodo();
     }
 });
 
-inputBar.addEventListener('keypress', (Event) => {
+inputBar.addEventListener('keypress', (e) => {
     // Execute only if user's input is not blank and if the key code is 13:
-    if (inputBar.value.length > 0 && Event.keyCode === 13) {
-        CreateNewListItem();
+    if (inputBar.value.length > 0 && e.keyCode === 13) {
+        handleSubmitTodo();
     }
 });
